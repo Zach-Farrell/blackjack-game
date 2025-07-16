bf881692c11e056600a2569def953e6c632d3a0a
Zach-Farrell <zachfarrell3@gmail.com>
2025-07-16 18:24:44 -0500

    added the simulation class functionality.

--- src/Simulation.java
+++ src/Simulation.java
@@ -20,6 +20,7 @@ public class Simulation {
 		startNewRound();
 	}
 
+	// reset the game state other than the shoe which stays constant until empty.
 	public void startNewRound() {
 		chips -= curBet;
 		player.reset();
@@ -27,13 +28,53 @@ public class Simulation {
 		deal();
 	}
 
-	public void hit() {
+	// the player hits and the next card in the shoe is dealt to them. Returns true
+	// if the player busts.
+	public boolean hit() {
 		player.addCard(shoe.getNextCard());
-		if (checkForBust()) {
-			// lose
+		return checkForBust(player);
+	}
+
+	// simulate the dealer when the player chooses the stand option.
+	public void stand() {
+		simulaterDealer();
+	}
+
+	public boolean doubleDown() {
+		return false;
+	}
+
+	// plays the hand of the dealer once the player is done. Return 0 if the dealer
+	// wins, 1 if the player's score beats the dealer's, 2 if the dealer busts, and
+	// 3 if the player had blackjack
+	private int simulaterDealer() {
+		if (player.isBlackJack() && !dealer.isBlackJack()) {
+			// if the player has a blackjack and the dealer does not then there is no need
+			// for dealer to draw cards.
+			chips += curBet * 1.5;
+			return 3;
+		}
+		while (true) {
+			if (dealer.getScore() < 17 || (dealer.getScore() == 17 && dealer.isSoft())) {
+				// dealer is not done drawing cards. Add card to hand and go to next iteration.
+				dealer.addCard(shoe.getNextCard());
+			} else if (checkForBust(dealer)) {
+				// dealer busts.
+				return 2;
+			} else {
+				// dealer is done drawing cards. Time to determine the winner.
+				if (player.determineWinner(dealer)) {
+					// player wins
+					return 1;
+				}
+				// no other ways for player to win, dealer wins.
+				return 0;
+			}
 		}
 	}
 
+	// simulates the initial deal of the game before the player chooses to hit,
+	// stand, double, etc...
 	private void deal() {
 		System.out.println("size of shoe: " + shoe.getNumCards());
 		player.addCard(shoe.getNextCard());
@@ -42,10 +83,16 @@ public class Simulation {
 		dealer.addCard(shoe.getNextCard());
 		System.out.println("size of shoe: " + shoe.getNumCards());
 		System.out.println("size of player hand: " + player.getNumCards());
+
+		if (player.isBlackJack()) {
+			simulaterDealer();
+			// BlackJack!
+		}
 	}
 
-	private boolean checkForBust() {
-		return false;
+	// return true if the hand is a bust (over 21)
+	private boolean checkForBust(Hand h) {
+		return h.getScore() > 21;
 	}
 
 }
